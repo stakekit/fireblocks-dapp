2cad112b1e476f0d1987627893e82a795ecc56db
Petar Todorovic <petar@omni.app>
2025-04-02 10:41:18 +0200

    feat(): bump deps, migrate to pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
-import React from "react"
-import { container, fixedContainer, widgetContainer } from "./style.css"
-import { Box } from "../components/atoms/box"
-import { Header } from "./header"
-import { Widget } from "./widget"
-import { HelpModals } from "./widget/help-modals"
-import { DottedBackground } from "../components/molecules/dotted-background"
-import { GradientContainer } from "../components/molecules/gradient-container"
+import React from "react";
+import { Box } from "../components/atoms/box";
+import { DottedBackground } from "../components/molecules/dotted-background";
+import { GradientContainer } from "../components/molecules/gradient-container";
+import { Header } from "./header";
+import { container, fixedContainer, widgetContainer } from "./style.css";
+import { Widget } from "./widget";
+import { HelpModals } from "./widget/help-modals";
 
 export default function Home() {
-  return (
-    <>
-      <Box className={container}>
-        <DottedBackground />
+	return (
+		<>
+			<Box className={container}>
+				<DottedBackground />
 
-        <Box className={fixedContainer}>
-          <GradientContainer />
-        </Box>
+				<Box className={fixedContainer}>
+					<GradientContainer />
+				</Box>
 
-        <Box className={widgetContainer}>
-          <Header />
-          <Box data-rk="widget-wrapper">
-            <Widget />
-          </Box>
-          <HelpModals />
-        </Box>
-      </Box>
-    </>
-  )
+				<Box className={widgetContainer}>
+					<Header />
+					<Box data-rk="widget-wrapper">
+						<Widget />
+					</Box>
+					<HelpModals />
+				</Box>
+			</Box>
+		</>
+	);
 }
--- src/styles/tokens/fonts.ts
+++ src/styles/tokens/fonts.ts
@@ -1,3 +1,3 @@
 export const fonts = {
-  body: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
-}
+	body: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+};
